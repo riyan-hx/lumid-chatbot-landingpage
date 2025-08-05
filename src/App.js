f89f39d006fa4f4acb2eee1f70a5bb2a26c589d0
riyan-hx <riyansk255@gmail.com>
2025-08-06 02:12:18 +0530

    Added Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './pages/Home';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Home/>
+        <Home />
       </header>
+      <Analytics /> {/* <-- This sends page view data to Vercel */}
     </div>
   );
 }
